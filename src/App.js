d4992e3b8394e73cecd4c77425d815396ffb045b
Saqib Azhar <saqibazharawan@gmail.com>
2022-03-27 01:25:27 +0500

    Component 2 added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
+import { DISHES } from './shared/dishes';
 import Menu from './components/MenuComponent';
+import { Component } from 'react';
 
-function App() {
+class  App extends Component {
+  
+    constructor (props){
+      super(props);
+      this.state ={
+        dishes :DISHES
+
+      };
+  }
+
+  render(){
   return (
     <div>
       <Navbar dark color='primary'>
         <div className='container'>
           <NavbarBrand href='/'>Ristornate COn Fusion</NavbarBrand></div> </Navbar>
-          <Menu />
+          <Menu dishes ={this.state.dishes}/>   
     </div>
   );
 }
-
+}
+// So, this way the dishes that I have defined in the state for
+// my App component is now made available as props to my menu component. 
 export default App;
